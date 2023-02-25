c0ac59fb321f1fb5ad13dc58927b33d359cb90bf
Hariharan Mohan <harishmohan138@gmail.com>
2023-02-25 18:42:55 +0530

    First commit....

---
+++ src/Styles/styledHome.js
@@ -0,0 +1,5 @@
+import tw, { styled } from "twin.macro";
+
+export const StyledHome = styled.div`
+  ${tw`text-4xl text-teal-300 font-bold`}
+`;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/App.js", "./src/Components/home.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
